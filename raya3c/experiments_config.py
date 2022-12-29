6603fdb2ab93d53a3f929d11884ab6a77c7d323b
Alessandro Canevaro <91014519+alessandro-canevaro@users.noreply.github.com>
2022-12-29 18:27:39 +0100

    Update experiments_config.py

--- raya3c/experiments_config.py
+++ raya3c/experiments_config.py
@@ -7,6 +7,6 @@ config = dict(
     batch_size = 32
 )
 
-config["run_name"] = f'PIE_05_2L_LAST_WP{config["wall_prob"]}_LR{config["lr"]}_BS{config["batch_size"]}_{config["maze_size"]}x{config["maze_size"]}_E{config["epochs"]}'
+config["run_name"] = f'TESTDEMO_05_2L_LAST_WP{config["wall_prob"]}_LR{config["lr"]}_BS{config["batch_size"]}_{config["maze_size"]}x{config["maze_size"]}_E{config["epochs"]}'
 config["models_dir"] = "./saved_models/"
 config["chk_point"] = f'/checkpoint_0000{config["epochs"]}'
